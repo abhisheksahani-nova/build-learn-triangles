198c0f894f807d69093ee61148fd587be8b72e84
Abhishek Sahani <abhisheksahani653@gmail.com>
2021-09-23 21:11:50 +0530

    Bug fix

--- scripts/calculateHypo.js
+++ scripts/calculateHypo.js
@@ -5,11 +5,17 @@ var result = document.querySelector(".output");
 calculateButton.addEventListener("click", calculateHypotenuse );
 
 function calculateHypotenuse() {
-  var sideSquareSum = Number(sides[0].value)*Number(sides[0].value) + Number(sides[1].value)*Number(sides[1].value);
-  console.log(sideSquareSum);
 
-  var hypotenuse = Math.sqrt(sideSquareSum);
-  console.log(hypotenuse);
+  if (Number(sides[0].value) < 0 || Number(sides[1].value) < 0) {
+    result.innerText = "Enter positive number" ;
 
-  result.innerText = "The length of hypotenuse is " + hypotenuse ;
+  }else{
+    var sideSquareSum = Number(sides[0].value)*Number(sides[0].value) + Number(sides[1].value)*Number(sides[1].value);
+    console.log(sideSquareSum);
+
+    var hypotenuse = Math.sqrt(sideSquareSum);
+    console.log(hypotenuse);
+
+    result.innerText = "The length of hypotenuse is " + hypotenuse ;
+  }
 }
